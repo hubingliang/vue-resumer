5c2dfbfce57f339c0dc6b1274217c818564c7df6
brown <1104524352@qq.com>
2017-07-19 23:27:17 +0800

    路径问题

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
         env: require('./prod.env'),
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: 'vue-resumer/dist/static',
-        assetsPublicPath: '/',
+        assetsSubDirectory: 'static',
+        assetsPublicPath: '/vue-resumer/dist/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
